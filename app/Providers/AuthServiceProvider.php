5e2330ca7267607237a073aff2ae398f7e4721cf
Jan Polak <jan.polak@onmyway.cz>
2023-10-25 12:21:03 +0200

    day 38 authorization, react app

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -22,5 +22,16 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Gate::define('admin', function($user) {
+
+            // decide if the user is an administrator (return true)
+            if (str_ends_with($user->email, '@data4you.cz')) {
+                return true;
+            }
+            // or not (return false)
+            else {
+                return false;
+            }
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\Api\TestController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\Admin\BookController;
+use App\Http\Controllers\Admin\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,3 +31,22 @@ Route::get('/api/test/array', [TestController::class, 'arrayResponse'])->name('a
 Route::get('/api/test/model', [TestController::class, 'modelResponse'])->name('model.response');
 Route::get('/api/test/collection', [TestController::class, 'collectionResponse'])->name('collection.response');
 
+
+// all admin routes
+Route::group([
+    'middleware' => 'can:admin'  // * settings of the group
+], function() {
+
+    // any routes defined in here will automatically get the settings (*) from the group
+
+    Route::get('/admin/books', [BookController::class, 'index']);
+
+    Route::get('/admin/users', [UserController::class, 'index']);
+
+});
+
+
+Route::get('/force-login/{user_id}', function($user_id) {
+    $user = User::findOrFail($user_id);
+    Auth::login($user);
+});
